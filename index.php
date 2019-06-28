042f7b40a9e114be7aa8d2e4e1e79c3cb809416f
Stupid Code <juststupidcode@gmail.com>
2019-06-28 23:01:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <title>Contoh Melakukan Query Tanpa Parameter</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <link rel="stylesheet" type="text/css" href="style.css">
     <script src='main.js'></script>
 </head>
 <body>
@@ -43,32 +42,11 @@ if($didapat == 0) {
 }
 else {
 echo "<p>Di Temukan $didapat record (baris).</p>";
-
-$no_urut=0;
-
-echo "<table class=table1>";
-echo "<tr>
-<th>No</th>
-<th>NIM</th>
-<th>NAMA</th>
-<th>LAHIR</th>
-<th>NILAI</th>
-</tr>";
-
-
+echo "<ol>";
 while ($buff = $stmt->fetch()){
-    $no_urut++;
-    echo "<tr>";
-    echo "<td>"$no_urut"</td>";
-    echo "<td>" . htmlentities($buff['nim']) . "</td>";
-    echo "<td>" . htmlentities($buff['nama']) . "</td>";
-    echo "<td>" . htmlentities($buff['lahir']) . "</td>";
-    echo "<td>" . htmlentities($buff['nilai']) . "</td>";
-    echo "</tr>";
-
+echo "<li>" . htmlentities($buff['nama']) . "</li>";
 };
-echo "</tr>";
-echo "</table>";
+echo "<ol>";
 };
 
 
